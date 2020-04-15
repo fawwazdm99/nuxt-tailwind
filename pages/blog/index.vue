872f77fabdac4a32e8e99d41c9b9d4556bb9478d
emub <em@ub.ac.id>
2020-04-15 16:00:36 +0700

    first commit

---
+++ pages/blog/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container mt-8">
+    <nuxt-link to="/">
+      <button
+        class="py-2 px-4 bg-green-500 rounded-lg text-white hover:bg-green-400 transition duration-300 focus:outline-none"
+      >
+        Back to home
+      </button>
+    </nuxt-link>
+    <h1>It's blog</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Blog",
+};
+</script>
+
+<style></style>
